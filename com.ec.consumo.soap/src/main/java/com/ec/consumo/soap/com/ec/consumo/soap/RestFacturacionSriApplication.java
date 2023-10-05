912c41d9bfa96d5423b68547a69c456ca93845e1
Vepita <35640194+Vepita@users.noreply.github.com>
2023-10-05 11:37:39 -0500

    Proyecto Inicial

---
+++ com.ec.consumo.soap/src/main/java/com/ec/consumo/soap/com/ec/consumo/soap/RestFacturacionSriApplication.java
@@ -0,0 +1,13 @@
+package com.ec.consumo.soap.com.ec.consumo.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFacturacionSriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFacturacionSriApplication.class, args);
+	}
+
+}
---
+++ com.ec.consumo.soap/src/test/java/com/ec/consumo/soap/com/ec/consumo/soap/RestFacturacionSriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.consumo.soap.com.ec.consumo.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFacturacionSriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
